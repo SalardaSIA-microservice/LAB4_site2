ad871c6593ba36ed813209cd26b30b4f04cc75e5
alishasalarda <salardaalishanicole6@gmail.com>
2023-05-27 07:58:48 +0800

    LAB2_FINAL

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 |
 | Here is where you can register all of the routes for an application.
 | It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.   
+| and give it the Closure to call when that URI is requested.
 |
 */
 
@@ -17,12 +17,15 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'api'], function () use ($router) {
-    $router->get('/users',['users' => 'UserController@getUsers']);
+$router->group(['prefix' => 'api'], function($router) {
+    $router->get('/users', 'UserController@showUsers');
+    $router->get('/users/{id}', 'UserController@showUser');
+    $router->post('/users', 'UserController@addUser');
+    $router->patch('/users/{id}', 'UserController@updateUser');
+    $router->delete('/users/{id}', 'UserController@deleteUser');
 });
 
-$router->get('/users', 'UserController@index'); 
-$router->post('/users', 'UserController@add'); 
-$router->get('/users/{id}', 'UserController@show'); 
-$router->put('/users/{id}', 'UserController@update'); 
-$router->delete('/users/{id}', 'UserController@delete'); 
+$router->group(['prefix' => 'api'], function($router) {
+    $router->get('/usersjob', 'UserJobController@showUsers');
+    $router->get('/usersjob/{id}', 'UserJobController@showUser');
+});
